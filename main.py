1f902946b0c2b507f730e68ebf7191a5d9c09293
Akeem Amusat <aoamusat@gmail.com>
2024-01-14 17:40:24 +0100

    Dropped column User ID from urls

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse
-from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from app.database.config import engine, Base, Session
-from app.database.models.url import URL, User
+from app.database.models.url import URL
 from app.validators.all import URLModel
 from fastapi.staticfiles import StaticFiles
 
@@ -19,11 +16,6 @@ templates = Jinja2Templates(directory="templates")
 
 # Static files configuration
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -41,14 +33,3 @@ def redirect(short_code):
     if url is None:
         return JSONResponse({"message": "Not Found"}, status_code=404)
     return RedirectResponse(url.long_url)
-
-
-@app.get("/user/{user_id}")
-def fetch_user(user_id: int):
-    try:
-        user = Session().query(User).filter(User.id == user_id).first()
-        if user is None:
-            return JSONResponse({"message": "User not found"}, status_code=404)
-        return JSONResponse({"user": user.email}, status_code=200)
-    except Exception as error:
-        return JSONResponse({"error": str(error)}, status_code=500)
