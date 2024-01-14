d26a41a64ebebafc9095ece9f7614085d5f7b541
Akeem Amusat <aoamusat@gmail.com>
2024-01-14 17:19:59 +0100

    Rate limit refactor: 100 Request per day

--- app/middlewares/rate_limit.py
+++ app/middlewares/rate_limit.py
@@ -6,42 +6,32 @@ from typing import Callable
 
 
 class RateLimitingMiddleware(BaseHTTPMiddleware):
-    # Rate limiting configurations
-    RATE_LIMIT_DURATION = timedelta(minutes=1)
-    RATE_LIMIT_REQUESTS = 3
+    # Rate limiting configurations: 100 requests per day
+    RATE_LIMIT_DURATION = timedelta(days=1)
+    RATE_LIMIT_REQUESTS = 100
 
     def __init__(self, app):
         super().__init__(app)
-        # Dictionary to store request counts for each IP
         self.request_counts = {}
 
     async def dispatch(self, request: Request, call_next: Callable) -> Response:
-        # Get the client's IP address
         client_ip = request.client.host
 
-        # Check if IP is already present in request_counts
         request_count, last_request = self.request_counts.get(
             client_ip, (0, datetime.min)
         )
 
-        # Calculate the time elapsed since the last request
         elapsed_time = datetime.now() - last_request
 
         if elapsed_time > self.RATE_LIMIT_DURATION:
-            # If the elapsed time is greater than the rate limit duration, reset the count
             request_count = 1
         else:
             if request_count >= self.RATE_LIMIT_REQUESTS:
-                # If the request count exceeds the rate limit, return a JSON response with an error message
                 return JSONResponse(
                     status_code=429,
                     content={"message": "Rate limit exceeded. Please try again later."},
                 )
             request_count += 1
-
-        # Update the request count and last request timestamp for the IP
         self.request_counts[client_ip] = (request_count, datetime.now())
-
-        # Proceed with the request
         response = await call_next(request)
         return response
